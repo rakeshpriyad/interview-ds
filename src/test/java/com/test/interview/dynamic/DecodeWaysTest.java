fb6bf0821c690cbffe82a9352772e65a52546434
Rajeshmca28 <rajesh.kumar.raj.mca@gmail.com>
2018-01-27 12:52:37 +0530

    first commit

---
+++ src/test/java/com/test/interview/dynamic/DecodeWaysTest.java
@@ -0,0 +1,11 @@
+package com.test.interview.dynamic;
+
+import org.junit.Test;
+
+public class DecodeWaysTest {
+    @Test
+    public void testDifferentCases() {
+        DecodeWays decodeWays = new DecodeWays();
+        System.out.println(decodeWays.numDecodings("20320"));
+    }
+}
---
+++ src/test/java/com/test/interview/string/ValidPalindromeTest.java
@@ -0,0 +1,14 @@
+package com.test.interview.string;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ValidPalindromeTest {
+
+    @Test
+    public void testDifferentCases() {
+        ValidPalindrome validPalindrome = new ValidPalindrome();
+        Assert.assertTrue(validPalindrome.isPalindrome("A man, a plan, a canal: Panama"));
+        Assert.assertFalse(validPalindrome.isPalindrome("race a car"));
+    }
+}
